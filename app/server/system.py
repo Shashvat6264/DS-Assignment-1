62d434f9d3a89cac07138e98667aa5ada28ddb98
sajalchh <sajalchh@gmail.com>
2023-01-26 02:48:08 +0530

    Minor changes in system configuration

--- app/server/system.py
+++ app/server/system.py
@@ -1,6 +1,14 @@
 from .controllers import *
 
-main_dq = DistributedQueue()
+from .database import get_db, engine
+
+from . import models
+
+models.Base.metadata.create_all(bind=engine)
+
+main_dq = DistributedQueue(database=get_db())
+main_dq.loadPersistence()
+
 
 
 
