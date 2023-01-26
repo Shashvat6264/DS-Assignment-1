2247c96230bd4384bdc92b63c2a1936ac75c8d42
Satvik-B <satvik.bansal2001@gmail.com>
2023-01-26 16:08:48 +0530

    Fixed routers to have query parameters

--- app/server/routers/consumer.py
+++ app/server/routers/consumer.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, Body, status, Query
 
 from ..system import main_dq
 from ..utils import *
@@ -13,8 +13,8 @@ async def register(topic: str = Body()):
         "consumer_id": customer_id
     }, status_code = status.HTTP_200_OK)
     
-@router.post('/consume', response_description="Consume message")
-async def consume(topic: str = Body(), consumer_id: int = Body()):
+@router.get('/consume', response_description="Consume message")
+async def consume(topic: str = Query(default=...), consumer_id: int = Query(default=...)):
     message = await main_dq.dequeue(topic_name = topic, id = consumer_id)
     return generic_Response(data = {
         "status": "success",
